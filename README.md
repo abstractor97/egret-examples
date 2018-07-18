7d49a37f8186d3b4a186f1ece2daf9fcbceb433a
ljcGitHub <523252797@qq.com>
2018-07-18 11:41:15 +0800

    新增: 锚点及旋转缩放

--- README.md
+++ README.md
@@ -8,5 +8,6 @@
 我找了好多教程，找不到，看官网示例学习，但是好像没一个完整的，或者缺了部分的代码，本文写demo学习。
 
 ## 目录
-* 1. 显示对象基本
-  * [1.1 基本的显示](https://github.com/ljcGitHub/koa2-server/tree/master/example/two/1)
+* [1. 显示对象基本](https://github.com/ljcGitHub/egret-examples/tree/master/one/1)
+  * [1.1 基本的显示](https://github.com/ljcGitHub/egret-examples/tree/master/one/1)
+  * [1.2 锚点及旋转缩放](https://github.com/ljcGitHub/egret-examples/tree/master/one/1)
--- one/1/src/Main.ts
+++ one/1/src/Main.ts
@@ -1,17 +1,6 @@
 
-class AnimModes {
-    public static ANIM_ROT: number = 0;
-    public static ANIM_SCALE: number = 1;
-}
-
 class Main extends egret.DisplayObjectContainer {
-    private static STEP_ROT: number = 3;
-    private static STEP_SCALE: number = .03;
     private _bird: egret.Bitmap;
-    private _txInfo: egret.TextField;
-    /// 用于记录当前的模式，模式切换通过触摸舞台触发
-    private _iAnimMode:number;
-    private _nScaleBase:number;
 
     public constructor() {
         super();
@@ -35,49 +24,5 @@ class Main extends egret.DisplayObjectContainer {
         this._bird.anchorOffsetY = this._bird.height / 2;
         this._bird.x = this.stage.stageWidth / 2;
         this._bird.y = this.stage.stageHeight * 0.618;
-
-        /// 提示信息
-        this._txInfo = new egret.TextField;
-        this.addChild(this._txInfo);
-        this._txInfo.size = 28;  /* private _txInfo:egret.TextField; */
-        this._txInfo.x = 50;
-        this._txInfo.y = 50;
-        this._txInfo.textAlign = egret.HorizontalAlign.LEFT;
-        this._txInfo.textColor = 0x000000;
-        this._txInfo.type = egret.TextFieldType.DYNAMIC;
-        this._txInfo.lineSpacing = 6;
-        this._txInfo.multiline = true;
-
-        this.launchAnimations();
-    }
-    private launchAnimations(): void {
-        this._iAnimMode = AnimModes.ANIM_ROT;
-        this.stage.addEventListener(egret.TouchEvent.TOUCH_TAP, () => {
-            this._iAnimMode = (this._iAnimMode + 1) % 3;
-        }, this);
-
-        this._nScaleBase = 0;
-
-        /// 根据当前模式调整旋转度数或缩放正弦基数形成相应动画
-        this.addEventListener(egret.Event.ENTER_FRAME, (evt: egret.Event) => {
-
-            /*** 本示例关键代码段开始 ***/
-            switch (this._iAnimMode) {
-                case AnimModes.ANIM_ROT:        /// 仅旋转
-                    this._bird.rotation += Main.STEP_ROT;
-                    break;
-                case AnimModes.ANIM_SCALE:        /// 仅缩放，缩放范围 0.5~1
-                    this._bird.scaleX = this._bird.scaleY = 0.5 + 0.5 * Math.abs(Math.sin(this._nScaleBase += Main.STEP_SCALE));
-                    break;
-            }
-            /*** 本示例关键代码段结束 ***/
-
-            this._txInfo.text =
-                "旋转角度:" + this._bird.rotation
-                + "\n缩放比例:" + this._bird.scaleX.toFixed(2)
-                + "\n\n轻触进入" + (["缩放", "静止", "旋转"][this._iAnimMode]) + "模式";
-
-            return false;  /// 友情提示： startTick 中回调返回值表示执行结束是否立即重绘
-        }, this);
     }
 }
